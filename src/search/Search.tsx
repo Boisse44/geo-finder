de54d172a356b0cc86fd607f16d04f2e826f825f
Antoine Boissinot <aboissinot@slack-corp.com>
2023-03-29 13:19:10 -0400

    Started implementing debounce

--- src/search/Search.tsx
+++ src/search/Search.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { keyboard } from "@testing-library/user-event/dist/keyboard";
 import { AxiosResponse } from "axios";
 import { FC, useState } from "react";
+import { useDebounce } from "use-debounce";
 import axios, { baseUrl, suggestionsResourceUrl } from '../api/AxiosConfig';
 
 interface SearchProps {
@@ -11,22 +12,22 @@ interface SearchProps {
 }
 
 const Search: FC = () => {
-    const [keyword, setKeyword] = useState("")
+    const [keyword, setKeyword] = useState("");
+    const debouncedSearchTerm = useDebounce(keyword, 500);
 
     const suggestions = async (keyword: string): Promise<any[]> => {
         const { data } = await axios.get(`${baseUrl}${suggestionsResourceUrl}?cityName=${keyword}`);
         return data;
     }
-    const { isLoading, status, data, error } = useQuery(['suggestions', keyword], () => suggestions(keyword), { retry: false });
+    const { isLoading, status, data, error } = useQuery(['suggestions', debouncedSearchTerm], () => suggestions(keyword), { retry: false, enabled: !!keyword});
 
 
     return (
         <div>
             <input onChange={(event) => setKeyword(event.target.value)}></input>
             <div>
-                { isLoading && <p>loading</p> }
                 {data && data.map((result: any) => {
-                    return (<p>{result}</p>)
+                    return (<p>test</p>)
                 })}
             </div>
         </div>
